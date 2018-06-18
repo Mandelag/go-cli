60a517f37e6034eccbc5b9ddcfd6b128b4c6e2d1
Keenan Gebze <keenan.gebze@gmail.com>
2018-06-19 00:32:45 +0700

    Delete many test case after aplying the builder pattern. (Trying) to apply builder pattern in building the map.

--- go-cli-spec.rb
+++ go-cli-spec.rb
@@ -1,40 +1,12 @@
 require_relative "go-cli"
+include Gocli
 
 RSpec.describe Map do
   describe "#Without initialization parameters," do
     it "create default map of 20 x 20 grid." do
-      map = Map.new
+      map = MapBuilder.new.build
       expect(map.height).to be(20)
       expect(map.width).to be(20)
-    end
-    it "place 5 randomly located drivers." do
-      map = Map.new
-      driver_count = map.spatial_objects.select {|obj| obj.instance_of? Driver}
-      expect(driver_count).to be(5)
-    end
-    it "place a randomly located go-cli user." do
-      map = Map.new
-      user_count = map.spatial_objects.select {|obj| obj.instance_of? User}
-      expect(user_count).to be(1)
-    end
-  end
-  
-  describe "#With one initialization parameter," do
-    it "create the map with grid with the same height and width" do
-      parameter = 20
-      map = Map.new(parameter)
-      expect(map.height).to be(parameter)
-      expect(map.width).to be(parameter)
-    end
-  end
-  
-  describe "#With two initialization parameter," do
-    it "create the map with grid with different height and width" do
-      height = 10
-      width = 20
-      map = Map.new(height, width)
-      expect(map.height).to be(height)
-      expect(map.width).to be(width)
     end
   end
   
--- go-cli.rb
+++ go-cli.rb
@@ -1,51 +1,72 @@
-class Map
-  attr_reader :height
-  attr_reader :width
-  
-  def initialize(height=20, width=20)
-    @height = height
-    @width = width
-    @map_objects = []
-  end
-  
-  def insert_object(object, x, y)
-    result = nil
-    if x < @width && x >= 0 && y < @height && y >= 0
-      result = MapObject.new(object, x, y)
-      @map_objects << result
+module Gocli
+  class MapBuilder
+    attr_accessor :width, :height, :map_objects
+    def initialize
+      @width = 20
+      @height = 20
+      @map_objects = []
     end
-    result
+    def build 
+      map = Map.new(@height, @width)
+      @map_objects.each do |obj|
+        map.insert_object(obj)
+      end
+      map
+    end
+  end
+
+  class Map
+    attr_reader :height
+    attr_reader :width
+    
+    def initialize(height=20, width=20)
+      @height = height
+      @width = width
+      @map_objects = []
+    end
+    
+    def insert_object(object, x=rand(@width), y=rand(@height))
+      result = nil
+      if x < @width && x >= 0 && y < @height && y >= 0
+        result = MapObject.new(object, x, y)
+        @map_objects << result
+      end
+      result
+    end
+    
+    def iterate_object
+      @map_objects.each {|objects|  yield(x)}
+    end
   end
   
-end
-
-class MapObject
-  attr_reader :object
-  attr_accessor :x
-  attr_accessor :y
+  class MapObject
+    attr_reader :object
+    attr_accessor :x
+    attr_accessor :y
+    
+    def initialize(object, x, y)
+      @object = object
+      @x = x
+      @y = y
+    end
+  end
   
-  def initialize(object, x, y)
-    @object = object
-    @x = x
-    @y = y
+  class Person
+    def initialize(name, phone, address)
+      @name = name
+      @phone = phone
+      @address = address
+    end
   end
-end
-
-class Person
-  def initialize(name, phone, address)
-    @name = name
-    @phone = phone
-    @address = address
-  end
-end
-
-class Driver < Person
-  def initialize(name, phone, address, bike)
-    super(name, phone, address)
-    @bike = bike
-  end
-end
-
-class Order
   
+  class Driver < Person
+    def initialize(name, phone, address, bike)
+      super(name, phone, address)
+      @bike = bike
+    end
+  end
+  
+  class Order
+    
+  end
 end
