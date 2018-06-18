1ffa4a93c2a1fb7f4733f17923f35aaaed2ad939
Keenan Gebze <keenan.gebze@gmail.com>
2018-06-18 23:33:06 +0700

    Rename SpatialObject to MapObject. Using composition over inheritance for the Driver and User object.

--- go-cli.rb
+++ go-cli.rb
@@ -5,19 +5,37 @@ class Map
   def initialize(height=20, width=20)
     @height = height
     @width = width
-    @spatial_objects = []
+    @map_objects = []
+  end
+  
+  def insert_object(object, x, y)
+    result = nil
+    if x < @width && x >= 0 && y < @height && y >= 0
+      result = MapObject.new(object, x, y)
+      @map_objects << result
+    end
+    result
+  end
+  
+end
+
+class MapObject
+  attr_reader :object
+  attr_accessor :x
+  attr_accessor :y
+  
+  def initialize(object, x, y)
+    @object = object
+    @x = x
+    @y = y
   end
 end
 
-class SpatialObject
+class Driver
   
 end
 
-class Driver < SpatialObject
-  
-end
-
-class User < SpatialObject
+class User
   
 end
 
