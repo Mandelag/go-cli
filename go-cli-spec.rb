e2e00876623a7cc45938f2a9511caa1d1d0c5e0f
Keenan Gebze <keenan.gebze@gmail.com>
2018-06-21 01:45:12 +0700

    Developed test case for save and load (serializing) mechanism and routing. Finally, some TDDs..

--- go-cli-spec.rb
+++ go-cli-spec.rb
@@ -29,3 +29,27 @@ RSpec.describe Map do
     end
   end
 end
+
+RSpec.describe Route do
+  describe "Initialization" do
+    it "Given a pair of coordinates (start and end), return a list of route coordinates." do
+      map_side = 20
+      start_x, start_y = rand(20), rand(20)
+      dest_x, dest_y = rand(20), rand(20)
+      map = Map.get_pre_populated(20, nil, nil)
+      route = map.route(start_x, start_y, dest_x, dest_y)
+      if route.found
+        expect(route.coordinates >= 2).to be(true)
+        expect(route.coordinates[0] == [start_x, start_y]).to be(true)
+        expect(route.coordinates[-1] == [dest_x, dest_y]).to be(true)
+        for i in 1...route.coordinates.length
+          front_coordinates = route.coordinates[i]
+          back_coordinates = route.coordinates[i-1]
+          horizontal_movement = (front_coordinates[0]-back_coordinates[0]).abs
+          vertical_movement = (front_coordinates[1]-back_coordinates[1]).abs
+          expect(horizontal_movement+vertical_movement).to be <= 1
+        end
+      end
+    end
+  end
+end
