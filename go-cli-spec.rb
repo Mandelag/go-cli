89a907acb24f7c4f3e79341841c247c91a86b5b3
Keenan Gebze <keenan.gebze@gmail.com>
2018-06-21 00:29:09 +0700

    Modify test. Starting to create the test case again to add new required functionality.

--- go-cli-spec.rb
+++ go-cli-spec.rb
@@ -4,21 +4,26 @@ include GoCli
 RSpec.describe Map do
   describe "#Without initialization parameters," do
     it "create default map of 20 x 20 grid." do
-      map = Map.new
+      map = Map.get_pre_populated(20, nil, nil)
       expect(map.height).to be(20)
       expect(map.width).to be(20)
     end
     it "randomly inserted map objects will never be outside the grid boundary." do
-      map = Map.new
-      drivers = [
-        Driver.new("Keenan", "081388439168", "Jl. Bukit Cinere", "Piaggio"),
-        Driver.new("Budi", "0811xxxxxxx", "Jl. Kober", "Vega-R"),
-        Driver.new("Anto", "0881xxxxxxx", "Jl. Bukit Timah", "Yamaha Mio"),
-        Driver.new("Grace", "0815xxxxxx", "Jl. Lipatan Bumi", "NMax"),
-        Driver.new("Widi", "0809xxxxxxx", "Jl. Bukit Cinere", "Royal Enfield")
-      ]
-      drivers.each {|driver| map.insert_object(driver, "D")}
+      map = Map.get_pre_populated(20, nil, nil)
       expect(map.map_objects.all? {|object| object.x >= 0 && object.x < map.width}).to be(true)
     end
   end
+  
+  describe "#Load, save, equal, and other related tests." do
+    it "Maps are equal when they have the same height, width, and MapObjects (not ordered)." do
+      
+    end
+    it "Test save and load" do
+      map = Map.new
+      map_json_string = map.save()
+      map_loaded = Map.load(map_json_string)
+      expect(map == map_loaded).to be(true)
+    end
+  end
+  
 end
