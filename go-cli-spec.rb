a02bd94a1313928d07f3cd1e1aaf06426a7063e7
Keenan Gebze <keenan.gebze@gmail.com>
2018-06-21 09:56:56 +0700

    Develop expected behavior test for saving and loading process.

--- go-cli-spec.rb
+++ go-cli-spec.rb
@@ -1,4 +1,5 @@
 require_relative "go-cli"
+require "json"
 include GoCli
 
 RSpec.describe Map do
@@ -15,16 +16,10 @@ RSpec.describe Map do
   end
   
   describe "#Load, save, equal, and other related tests." do
-    it "Maps are equal when they have the same height, width, and MapObjects (not ordered)." do
-      map = Map.get_pre_populated(20, nil, nil)
-      str = Map.save(map)
-      map_loaded = Map.load(str)
-      expect(map == map_loaded).to be(true)
-    end
     it "Test save and load." do
       map = Map.new
-      map_json_string = map.save()
-      map_loaded = Map.load(map_json_string)
+      map_string = map.save()
+      map_loaded = Map.load(map_string)
       expect(map == map_loaded).to be(true)
     end
   end
